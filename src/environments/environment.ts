4c95c86beb8c4f6e402a649a64a39404fb175d4a
wbarnie <webarnie18@gmail.com>
2020-12-15 13:23:23 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCenoeOAL4UI_chQUuyWEabmIjOARnOWH4',
+    authDomain: 'fir-course-de625.firebaseapp.com',
+    databaseURL: 'https://fir-course-de625.firebaseio.com',
+    projectId: 'fir-course-de625',
+    storageBucket: 'fir-course-de625.appspot.com',
+    messagingSenderId: '433291498896',
+    appId: '1:433291498896:web:a6b2e8adbb98cbb6f90f15',
+    measurementId: 'G-QRT8TRVTK6'
+  }
+};
